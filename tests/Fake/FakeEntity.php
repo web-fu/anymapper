366e5734ccc36f72afd7b04ceddb467ba49951ad
web-fu <info@web-fu.it>
2022-11-18 14:28:58 +0100

    Test : changed namespace FakeEntity created for tests

---
+++ tests/Fake/FakeEntity.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebFu\Tests\Fake;
+
+class FakeEntity
+{
+    public mixed $public_1;
+    public mixed $public_2;
+    public mixed $public_3;
+
+    public function __construct()
+    {
+    }
+
+    public function getPublic(): void
+    {
+    }
+
+    public function isPublic(): void
+    {
+    }
+
+    public function __get(string $key): void
+    {
+        $this->getPrivate();
+    }
+
+    public function getty(): void
+    {
+    }
+
+    protected function getProtected(): void
+    {
+    }
+
+    private function getPrivate(): void
+    {
+    }
+
+    public function setPublic(): void
+    {
+    }
+
+    public function __set(string $key, mixed $value): void
+    {
+    }
+}
---
+++ tests/Unit/Mapper/ClassAnalyzerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebFu\Tests\Unit\Mapper;
+
+use PHPUnit\Framework\TestCase;
+use WebFu\Mapper\ClassAnalyzer;
+use WebFu\Tests\Fake\FakeEntity;
+
+class ClassAnalyzerTest extends TestCase
+{
+    public function testGetConstructor(): void
+    {
+        $class = new FakeEntity();
+        $classAnalyzer = new ClassAnalyzer($class);
+        $constructor = $classAnalyzer->getConstructor();
+
+        $this->assertNotNull($constructor);
+    }
+
+    public function testGetGetters(): void
+    {
+        $class = new FakeEntity();
+        $classAnalyzer = new ClassAnalyzer($class);
+        $getters = $classAnalyzer->getGetters();
+
+        $this->assertEquals([
+            'getPublic',
+            'isPublic',
+            '__get',
+        ], array_keys($getters));
+    }
+
+    public function testGetSetters(): void
+    {
+        $class = new FakeEntity();
+        $classAnalyzer = new ClassAnalyzer($class);
+        $setters = $classAnalyzer->getSetters();
+
+        $this->assertEquals([
+            'setPublic',
+            '__set',
+        ], array_keys($setters));
+    }
+
+    public function testGetProperties(): void
+    {
+        $class = new FakeEntity();
+        $classAnalyzer = new ClassAnalyzer($class);
+        $properties = $classAnalyzer->getProperties();
+
+        $this->assertEquals([
+            'public_1',
+            'public_2',
+            'public_3',
+        ], array_keys($properties));
+    }
+}
