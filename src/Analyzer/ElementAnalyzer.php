9d78d3de9dad8a42cb9585d2053dcdadf0a4c110
web-fu <info@web-fu.it>
2022-12-16 12:15:44 +0100

    Indexes can be numeric

--- src/Analyzer/ElementAnalyzer.php
+++ src/Analyzer/ElementAnalyzer.php
@@ -7,12 +7,12 @@ namespace WebFu\Analyzer;
 class ElementAnalyzer
 {
     public function __construct(
-        private readonly string      $name,
+        private readonly string|int  $name,
         private readonly ElementType $type,
     ) {
     }
 
-    public function getName(): string
+    public function getName(): string|int
     {
         return $this->name;
     }
