25f36a5ff6b646c516e82a7f93a9b433718b0c48
web-fu <info@web-fu.it>
2022-12-23 10:41:03 +0100

    AnyMapper tested map as

--- tests/Unit/Mapper/AnyMapperTest.php
+++ tests/Unit/Mapper/AnyMapperTest.php
@@ -10,7 +10,7 @@ use WebFu\Tests\Fake\FakeEntity;
 
 class AnyMapperTest extends TestCase
 {
-    public function testMap(): void
+    public function testMapInto(): void
     {
         $class = new FakeEntity();
 
@@ -19,6 +19,21 @@ class AnyMapperTest extends TestCase
             'public' => 'public',
             'bySetter' => 'bySetter',
         ])->into($class);
+
+        $this->assertSame('byConstructor is set by constructor', $class->getByConstructor());
+        $this->assertSame('public', $class->public);
+        $this->assertSame('bySetter is set by setter', $class->getBySetter());
+    }
+
+    public function testMapAs(): void
+    {
+        $class = (new AnyMapper())->map([
+            'byConstructor' => 'byConstructor',
+            'public' => 'public',
+            'bySetter' => 'bySetter',
+        ])->as(FakeEntity::class);
+
+        $this->assertInstanceOf(FakeEntity::class, $class);
 
         $this->assertSame('byConstructor is set by constructor', $class->getByConstructor());
         $this->assertSame('public', $class->public);
