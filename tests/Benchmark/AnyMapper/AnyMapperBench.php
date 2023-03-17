3e06f47f2b83930c80dcb55084c907431a3f43db
web-fu <info@web-fu.it>
2023-03-17 14:47:32 +0100

    PHPBench library added to dev

---
+++ tests/Benchmark/AnyMapper/AnyMapperBench.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebFu\Tests\Benchmark\AnyMapper;
+
+class AnyMapperBench
+{
+    /**
+     * @Revs(1000)
+     */
+    public function benchMapInto(): void
+    {
+        $source = [
+            'foo' => 'I am foo',
+            'bar' => 'I am bar',
+        ];
+
+        $destination = new class {
+            private string $foo;
+            public string $bar;
+
+            public function setFoo(string $foo): self
+            {
+                $this->foo = $foo . ' and I was set in a setter';
+                return $this;
+            }
+
+            public function getFoo(): string
+            {
+                return $this->foo;
+            }
+        };
+
+        (new \WebFu\AnyMapper\AnyMapper())
+            ->map($source)
+            ->into($destination);
+    }
+}
