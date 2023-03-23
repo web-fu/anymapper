4bd7c16f50dd681ba23d00b291f8ed9f775f2715
web-fu <info@web-fu.it>
2023-03-23 16:26:47 +0100

    DataCastingStrategy 100% coverage

--- tests/Unit/AnyMapper/Strategy/DataCastingStrategyTest.php
+++ tests/Unit/AnyMapper/Strategy/DataCastingStrategyTest.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace WebFu\Tests\Unit\AnyMapper\Strategy;
 
 use PHPUnit\Framework\TestCase;
+use WebFu\AnyMapper\MapperException;
 use WebFu\AnyMapper\Strategy\DataCastingStrategy;
 use DateTime;
 use WebFu\Reflection\ReflectionTypeExtended;
+use WebFu\Tests\Fixture\Foo;
 
 class DataCastingStrategyTest extends TestCase
 {
@@ -18,5 +20,16 @@ class DataCastingStrategyTest extends TestCase
         $actual = $strategy->cast('2022-12-01', new ReflectionTypeExtended([DateTime::class]));
 
         $this->assertEquals(new DateTime('2022-12-01'), $actual);
+    }
+
+    public function testCastFail(): void
+    {
+        $strategy = new DataCastingStrategy();
+        $strategy->allow('string', Foo::class);
+
+        $this->expectException(MapperException::class);
+        $this->expectExceptionMessage('Cannot convert type string into any of the following types: '. DateTime::class);
+
+        $strategy->cast('2022-12-01', new ReflectionTypeExtended([DateTime::class]));
     }
 }
