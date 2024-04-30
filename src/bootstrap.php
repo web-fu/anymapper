9f8bfe855f47c936de67bab508ee7805801788c0
web-fu <info@web-fu.it>
2024-04-30 18:39:16 +0200

    CS fix

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -12,8 +12,8 @@ declare(strict_types=1);
  */
 
 const FUNCTIONS = [
-    'WebFu\\Internal\\camelcase_to_underscore' => 'Internal/camelcase_to_underscore.php',
-    'WebFu\\Internal\\get_type'                => 'Internal/get_type.php',
+    'WebFu\Internal\camelcase_to_underscore' => 'Internal/camelcase_to_underscore.php',
+    'WebFu\Internal\get_type'                => 'Internal/get_type.php',
 ];
 
 foreach (FUNCTIONS as $function => $file) {
